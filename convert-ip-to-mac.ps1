84f46b61878b25ba6b61267a7c06af64ef4cc108
TINTANET <cedric.tintanet@gmail.com>
2017-09-26 22:02:33 +0200

    convert last two bytes of ipv4address to hexa value for mac address

---
+++ convert-ip-to-mac.ps1
@@ -0,0 +1,31 @@
+﻿
+write-Host "("
+for($i=1;$i -lt 16; $i++) {
+    $macString = "08002701"
+    $macdhcp = "08-00-27-01"
+    $stringIP = (Get-DnsServerResourceRecord -ComputerName tikal -ZoneName "ads.intra.inist.fr" -Name "form$i").RecordData.IPV4address.IPAddressToString
+    #$stringIP
+    $tabIPv4 = $stringIP.split('.')
+    
+    for($j=2;$j -lt $tabIPV4.Length;$j++) {
+        $octet = [System.Convert]::ToInt32($tabIPv4[$j])
+        if($octet -lt 16) {
+            $macString += "0"+ [System.convert]::ToString($octet,16)
+            $macdhcp += "-0"+[System.convert]::ToString($octet,16)
+        }
+        else {
+            $macString += [System.convert]::ToString($octet,16)
+            $macdhcp += "-"+[System.convert]::ToString($octet,16)
+        }
+    }
+    #write-host $macstring
+    write-Host "$macdhcp,"
+    #$endIP = [System.convert]::ToString($i,10)
+    #if($i -lt 10) {
+    #    $endIP = "0"+$endIP
+    #}
+        
+    #write-Host "Add-DhcpServerv4Reservation -ScopeId 172.16.96.0 -IPAddress "172.16.105.1$endIP" -Name "VKFORM$i.ads.intra.inist.fr" -Type Both -ClientId "3C-97-0E-E4-B2-75" -ComputerName vpads-dc1"
+}
+write-Host ")"
+
