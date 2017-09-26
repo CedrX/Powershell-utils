3c0358f4084ebb3153b261001e2fcf7db20f5e98
TINTANET <cedric.tintanet@gmail.com>
2017-09-26 22:08:29 +0200

    parse web page to find content

---
+++ parse-webpage.ps1
@@ -0,0 +1,16 @@
+﻿$url = "https://the.earth.li/~sgtatham/putty/latest/w64/"
+
+$webproxy = "http://proxyout.contoso.com:8080"
+$proxy = new-object System.Net.WebProxy
+$proxy.Address = $webproxy
+
+$request = [System.Net.WebRequest]::Create($url)
+$request.Method="Get"
+$reponse = $request.GetResponse()
+$reponseStream = $reponse.GetResponseStream()
+$readstream = new-object System.IO.StreamReader($reponseStream);
+$pageContent = $readstream.ReadToEnd()
+$pageContent.GetType()
+$pageContent | select-string -Pattern ".msi"
+
+
